3fe46725df6c9c9de84d357c30a3111588d4250d
Micheal Parks <michealparks1989@gmail.com>
2023-11-20 23:36:51 -0500

    add base to paths

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
+<script lang='ts'>
+  import { base } from '$app/paths'
+</script>
+
 <main>
   <h1>Experiments</h1>
   <ul>
-    <li><a href='./batched1'>Batched Mesh 1</a></li>
-    <li><a href='./batched2'>Batched Mesh 2</a></li>
+    <li><a href='{base}/batched1'>Batched Mesh 1</a></li>
+    <li><a href='{base}/batched2'>Batched Mesh 2</a></li>
   </ul>
-  
 </main>
 
 <style>
